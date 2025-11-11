b53af9f3d6ee2abc266f012a2bcb312cecc1873d
Jack Gunn <jackhgunn0@gmail.com>
2025-11-11 10:31:45 -0500

    Added script to preprocess and extract features

---
+++ script.py
@@ -0,0 +1,18 @@
+import pandas as pd
+from pathlib import Path
+import sys
+
+sys.path.append(str(Path(__file__).resolve().parent / "src"))
+
+from preprocess import preprocess_text
+from feature_extraction import extract_features
+
+dataset_path = Path(__file__).resolve().parent / "fake-reviews.csv"
+dataset_df = pd.read_csv(dataset_path)
+
+dataset_df["cleaned_text"] = dataset_df["text_"].apply(preprocess_text)
+POS_Tagging = True
+dataset_df = extract_features(dataset_df, include_pos=POS_Tagging)
+
+processed_dataset_path = Path(__file__).resolve().parent / "processed-fake-reviews.csv"
+dataset_df.to_csv(processed_dataset_path, index=False)
